b4ec404b1f3585b97a100c7a91b0988c12d0a799
M5-hash <81691321+M5-hash@users.noreply.github.com>
2021-04-23 03:39:54 +0200

    Server Prototyp

--- Server.java
+++ Server.java
@@ -3,65 +3,90 @@ import java.io.*;
 import java.util.Enumeration;
 
 class Server {
-    // Server-Seite eines sehr einfachen Chat-Programms mit Sockets.
-    // (Anstelle von "throws IOException" sollte man Ausnahmen besser
-    // gezielt mit try-catch auffangen.)
-    public static void main (String [] args) throws IOException {
-	// Verwendete Portnummer.
-	final int port = 50000;
 
-	// Server-Socket erzeugen und an diesen Port binden.
-	ServerSocket ss = new ServerSocket(port);
+    final int port;
+    private ServerSocket ss;
+    private Socket s;
+    private BufferedReader in;
+    private Writer out;
+    private BufferedReader usr;
+    private String line;
 
-		Enumeration<NetworkInterface> nis =
-				NetworkInterface.getNetworkInterfaces();
-		while (nis.hasMoreElements()) {
-			NetworkInterface ni = nis.nextElement();
-			Enumeration<InetAddress> ias = ni.getInetAddresses();
-			while (ias.hasMoreElements()) {
-				InetAddress ia = ias.nextElement();
-				if (!ia.isLoopbackAddress()) {
-					System.out.print(" " + ia.getHostAddress());
-				}
-			}
-		}
+    public Server(int Port) throws IOException, NullPointerException {
 
-	// Auf eine Client-Verbindung warten und diese akzeptieren.
-	// Als Resultat erhält man ein "normales" Socket.
-	System.out.println("Waiting for client connection ...");
-	Socket s = ss.accept();
-	System.out.println("Connection established.");
+        this.port = 50000;
+        this.ss = new ServerSocket(port);
 
-	// Ein- und Ausgabestrom des Sockets ermitteln
-	// und als BufferedReader bzw. Writer verpacken
-	// (damit man zeilen- bzw. zeichenweise statt byteweise arbeiten kann).
-	BufferedReader in =
-		new BufferedReader(new InputStreamReader(s.getInputStream()));
-	Writer out = new OutputStreamWriter(s.getOutputStream());
 
-	// Standardeingabestrom ebenfalls als BufferedReader verpacken.
-	BufferedReader usr = 
-			new BufferedReader(new InputStreamReader(System.in));
+        IP_Ausgabe();
+        System.out.println("Waiting for client connection ...");
+        this.s = this.ss.accept();
+        System.out.println("Connection established.");
+        this.in = new BufferedReader(new InputStreamReader(this.s.getInputStream()));
+        this.out = new OutputStreamWriter(this.s.getOutputStream());
+        this.usr = new BufferedReader(new InputStreamReader(System.in));
+        ServerCommunicate();
+        KillSocket();
 
-	// Abwechselnd vom Socket lesen und auf den Bildschirm schreiben
-	// bzw. vom Benutzer lesen und ins Socket schreiben.
-	// Abbruch bei EOF vom Socket bzw. bei EOF oder Leerzeile vom Benutzer.
-	while (true) {
-	    String line = in.readLine();
-	    if (line == null) break;
-	    System.out.println("<<< " + line);
+    }
 
-	    System.out.print(">>> ");
-	    line = usr.readLine();
-	    if (line == null || line.equals("")) break;
-	    out.write(String.format("%s%n", line));
-	    out.flush();
-	    // flush sorgt dafür, dass der Writer garantiert alle Zeichen
-	    // in den unterliegenden Ausgabestrom schreibt.
-	}
+    public void IP_Ausgabe() throws IOException {
+        Enumeration<NetworkInterface> nis =
+                NetworkInterface.getNetworkInterfaces();
+        while (nis.hasMoreElements()) {
+            NetworkInterface ni = nis.nextElement();
+            Enumeration<InetAddress> ias = ni.getInetAddresses();
+            while (ias.hasMoreElements()) {
+                InetAddress ia = ias.nextElement();
+                if (!ia.isLoopbackAddress()) {
+                    System.out.print(" " + ia.getHostAddress());
+                }
+            }
+        }
+    }
 
-	// EOF ins Socket "schreiben".
-	s.shutdownOutput();
-	System.out.println("Connection closed.");
+    public void Send(String input) throws IOException{
+        this.out.write(String.format("%s%n", input));
+        this.out.flush();
+    }
+
+    public String Receive() throws IOException{
+        System.out.println(this.line);
+        return this.line;
+    }
+
+    public boolean out_check() throws IOException {
+        this.line = this.usr.readLine();
+        if (this.line == null || this.line.equals("")) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public boolean in_check() throws IOException {
+        this.line = this.in.readLine();
+        if (this.line == null || this.line.equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+    public void ServerCommunicate() throws IOException {
+        while (true) {
+            if(in_check() == false) break;
+
+            Receive();
+
+            if(out_check() == false) break;
+
+            Send("aa");
+        }
+
+    }
+
+    public void KillSocket() throws IOException{
+        this.s.shutdownOutput();
+        System.out.println("Connection closed.");
     }
 }
