946408cd7b753554aacf0f25dc4f93f15edc6b49
Tom Rathgeb <tom.rathgeb@studmail.htw-aalen.de>
2021-06-02 13:01:52 +0200

    Habe das Programm aus der main in der Konstruktor verschoben

--- src/SpielWindow.java
+++ src/SpielWindow.java
@@ -19,67 +19,6 @@ public class SpielWindow extends JPanel {
 
     public static void main(String[] args) {                     //Startet das Programm und erstellt das Window
 
-        /*final int[] frame_height = new int[1];
-        final int[] frame_width = new int[1];
-        int bottom_gap = 80;
-
-
-        JFrame frame = new JFrame("Schiffe versenken");
-
-        frame.setSize(new Dimension(1600, 1200));
-        frame.setLocation(new Point(1400, 20));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.add(Singleplayer);
-
-
-        Singleplayer.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                System.out.println("Du hast den schönen SinglePlayer Knopf berührt");
-                TilePainter tile = new TilePainter(field_size);
-                frame.remove(Singleplayer);
-                frame.getContentPane().add(tile);
-                frame_height[0] = frame.getHeight();
-                frame_width[0] = frame.getWidth();
-
-
-                Timer timer = new Timer(55, new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-
-                        //System.out.println("Der Timer wurde gestartet");
-
-                        if (frame_height[0] != frame.getHeight() || frame_width[0] != frame.getWidth()) {
-                            if (frame.getWidth() > frame.getHeight()) {
-                                frame_height[0] = frame.getHeight() - 30;
-                                frame_width[0] = (frame.getHeight() - 100);
-                                //frame.setSize(frame.getHeight(), frame.getHeight() + bottom_gap);
-
-                            } else if (frame.getWidth() < frame.getHeight()) {
-                                frame_height[0] = frame.getWidth() - 30;
-                                frame_width[0] = frame.getWidth() - 100;
-                                //frame.setSize(frame.getWidth(), frame.getWidth());
-                            }
-
-                            TileSize.setTile_Height(Math.min(frame_height[0], frame_width[0]) / field_size);
-                            TileSize.setTile_Width(Math.min(frame_height[0], frame_width[0]) / field_size);
-
-
-                        }
-                        tile.repaint(); //Der beste Command, der von der Menschheit erfunden wurde
-                        tile.revalidate();
-                    }
-                });
-                //frame.getContentPane().add(new SpielWindow());
-                timer.start();
-                //frame.setSize(TileSize.Tile_Height * field_size + 120 , TileSize.Tile_Height * field_size + 120 + 20/10);
-
-            }
-        });*/
-
-
         SpielWindow Hallo = new SpielWindow() ;
 
     }
