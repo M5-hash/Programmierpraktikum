ef7f791d81c0c603c598e8374efde5cf43b606aa
SchWer <julian.haenle@gmx.de>
2021-06-16 12:59:35 +0200

    Computer Spieler Dateien hinzugefügt (noch nicht implementiert). Beschossenes Wasser hinzugefügt.

---
+++ src/ComPlayerEasy.java
@@ -0,0 +1,16 @@
+package src;
+
+/**
+ * Computer-Spieler mit Schwierigkeitsgrad Einfach
+ * Schiffssetzung und Schüsse hauptsächlich zufällig
+ */
+public class ComPlayerEasy extends ComPlayer{
+    public ComPlayerEasy(int rows, int[] ships) {
+        super(rows, ships);
+    }
+
+    @Override
+    protected void setShips() {
+        //TODO
+    }
+}
