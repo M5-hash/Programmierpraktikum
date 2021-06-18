3536bd00c0dc28a4520ffc6cd8e0a450da94b1cc
SchWer <julian.haenle@gmx.de>
2021-06-18 16:43:39 +0200

    Computer-Gegner Schiff-Platzierung und Wrapper für loadGame

--- src/ComPlayerEasy.java
+++ src/ComPlayerEasy.java
@@ -1,16 +1,13 @@
 package src;
 
+import java.util.Random;
+
 /**
  * Computer-Spieler mit Schwierigkeitsgrad Einfach
  * Schiffssetzung und Schüsse hauptsächlich zufällig
  */
 public class ComPlayerEasy extends ComPlayer{
-    public ComPlayerEasy(int rows, int[] ships) {
+    public ComPlayerEasy(int rows, int[] ships) throws Exception{
         super(rows, ships);
-    }
-
-    @Override
-    protected void setShips() {
-        //TODO
     }
 }
