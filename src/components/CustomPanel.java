4ccec09848bc1a55687d693da8dbe060ebfba114
Tom Rathgeb <tom.rathgeb@studmail.htw-aalen.de>
2021-07-04 14:52:24 +0200

    MIt Classic Theme und captain Iglo

--- src/components/CustomPanel.java
+++ src/components/CustomPanel.java
@@ -9,21 +9,25 @@ import static src.config.selectedTheme ;
 
 public class CustomPanel extends JPanel {
 
-    private BufferedImage background;
+    private BufferedImage givenbackground;
 
     public CustomPanel(BufferedImage image){
-        this.background = image;
+        this.givenbackground = image;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        BufferedImage background ;
         if(!selectedTheme.equals("Pokemon")){
             Bildloader Bild = new Bildloader() ;
             background = Bild.BildLoader("src/Images/NavalBackground.jpg") ;
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+        } else {
+
+            g.drawImage(givenbackground, 0, 0, getWidth(), getHeight(), this);
         }
 
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
     }
 }
