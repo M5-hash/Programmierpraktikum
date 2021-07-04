3c601535e28ba87307b35bcaa044bcd55af5bc38
Mitchell <may-pascalrene@web.de>
2021-07-04 19:40:51 +0200

    a

--- src/components/PanelSize.java
+++ src/components/PanelSize.java
@@ -3,50 +3,143 @@ package src.components;
 import src.ImageLoader;
 import src.Server;
 import src.SpielWindow;
-
 import javax.swing.*;
 import javax.swing.plaf.metal.MetalButtonUI;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import static src.FontLoader.Pokemon;
 import static src.config.*;
 
+/**
+ *
+ */
 public class PanelSize extends JPanel {
 
     private final BufferedImage background;
 
+    /**
+     * Temporäre Feldgröße
+     */
     int temp_fieldsize = fieldsize;
+    /**
+     * Temporäre Anzahl an Feldern
+     */
     int temp_fields = temp_fieldsize * temp_fieldsize;
+    /**
+     * Temporäre Anzahl an size 2 Ships
+     */
     int temp_size2 = size2;
+    /**
+     * Temporäre Anzahl an size 3 Ships
+     */
     int temp_size3 = size3;
+    /**
+     * Temporäre Anzahl an size 4 Ships
+     */
     int temp_size4 = size4;
+    /**
+     * Temporäre Anzahl an size 5 Ships
+     */
     int temp_size5 = size5;
+    /**
+     * Temporäre Anzahl an Feldern, die noch nicht besetzt sind
+     */
     int temp_fieldsleft = 16;
 
+    /**
+     * Constraints für das GridbagLayout
+     */
     GridBagConstraints constraints;
+    /**
+     * GridBaglayout der Panelsize <br>
+     */
     GridBagLayout menuLayout;
+    /**
+     * Setzt alle temporären Werte auf Default Werte zurück <br>
+     * Ruft das vorherige Window auf
+     */
     JButton buttonCancel;
+    /**
+     * Setzt die Werte von fieldsize, size2, size3, size4, size5 auf die entsprechenden temporären Werte
+     */
     JButton buttonApply;
+    /**
+     * Slider für die Feldgröße
+     */
     JSlider menuSlider1;
+    /**
+     * Slider für size2
+     */
     JSlider menuSlider2;
+    /**
+     * Slider für size3
+     */
     JSlider menuSlider3;
+    /**
+     * Slider für size4
+     */
     JSlider menuSlider4;
+    /**
+     * Slider für size5
+     */
     JSlider menuSlider5;
+    /**
+     * Zeigt den Wert für temp_fieldsleft an
+     */
     JLabel menuCounter;
+    /**
+     * Label für den Slider der Feldgräße
+     */
     JLabel menuSlidername1;
+    /**
+     * Label für size2 Slider
+     */
     JLabel menuSlidername2;
+    /**
+     * Label für size3 Slider
+     */
     JLabel menuSlidername3;
+    /**
+     * Label für size4 Slider
+     */
     JLabel menuSlidername4;
+    /**
+     * Label für size5 Slider
+     */
     JLabel menuSlidername5;
+    /**
+     * Textfeld für Feldgröße
+     */
     JTextField textField1;
+    /**
+     * Textfeld für size2
+     */
     JTextField textField2;
+    /**
+     * Textfeld für size 3
+     */
     JTextField textField3;
+    /**
+     * Textfeld für size 4
+     */
     JTextField textField4;
+    /**
+     * Textfeld für size 5
+     */
     JTextField textField5;
+    /**
+     * Panel für buttonApply und buttonCancel
+     */
     JPanel buttonPanel;
 
+    /**
+     * Erstellung der Components im Startmenü
+     * @param menuFrame JFrame auf dem die Components erstellt werden
+     * @param image Hintergrundbild des Panels
+     * @param previousPanel menuPanel des zuvor zusehenden Panels
+     * @param menuPanel Aktuelles menuPanel des Sizemenüs
+     */
     public PanelSize(JFrame menuFrame, BufferedImage image, JPanel previousPanel, JPanel menuPanel) {
         this.background = image;
         setOpaque(false);
@@ -73,23 +166,30 @@ public class PanelSize extends JPanel {
         menuCounter.setFont(Pokemon);
         makeConstraints(menuCounter, 1, 1, 3);
 
+        if(selectedTheme.equals("Pokemon")){
+            menuSlidername2 = new JLabel("<html><body>   Size  2<br>Pokemon</body></html>");
+            menuSlidername3 = new JLabel("<html><body>   Size 3<br>Pokemon</body></html>");
+            menuSlidername4 = new JLabel("<html><body>   Size 4<br>Pokemon</body></html>");
+            menuSlidername5 = new JLabel("<html><body>   Size 5<br>Pokemon</body></html>");
+        } else {
+            menuSlidername2 = new JLabel("<html><body>   Size  2<br>Ship</body></html>");
+            menuSlidername3 = new JLabel("<html><body>   Size 3<br>Ship</body></html>");
+            menuSlidername4 = new JLabel("<html><body>   Size 4<br>Ship</body></html>");
+            menuSlidername5 = new JLabel("<html><body>   Size 5<br>Ship</body></html>");
+        }
         menuSlidername1 = new JLabel("Fieldsize");
         menuSlidername1.setFont(Pokemon.deriveFont(11f));
         makeConstraints(menuSlidername1, 1, 2, 1);
 
-        menuSlidername2 = new JLabel("<html><body>   Size  2<br>Pokemon</body></html>");
         menuSlidername2.setFont(Pokemon.deriveFont(11f));
         makeConstraints(menuSlidername2, 1, 3, 1);
 
-        menuSlidername3 = new JLabel("<html><body>   Size 3<br>Pokemon</body></html>");
         menuSlidername3.setFont(Pokemon.deriveFont(11f));
         makeConstraints(menuSlidername3, 1, 4, 1);
 
-        menuSlidername4 = new JLabel("<html><body>   Size 4<br>Pokemon</body></html>");
         menuSlidername4.setFont(Pokemon.deriveFont(11f));
         makeConstraints(menuSlidername4, 1, 5, 1);
 
-        menuSlidername5 = new JLabel("<html><body>   Size 5<br>Pokemon</body></html>");
         menuSlidername5.setFont(Pokemon.deriveFont(11f));
         makeConstraints(menuSlidername5, 1, 6, 1);
 
@@ -221,7 +321,7 @@ public class PanelSize extends JPanel {
                 if (x == 0) {
                     onlineCom = false;
                     menuPanel.setVisible(false);
-                    //menuFrame.dispose();
+                    menuFrame.dispose();
                     // Create SpielWindow and display it
                     try {
                         SpielFeld1 = 0;
@@ -233,7 +333,7 @@ public class PanelSize extends JPanel {
                 } else if (x == 1) {
                     onlineCom = true;
                     menuPanel.setVisible(false);
-                    //menuFrame.dispose();
+                    menuFrame.dispose();
                     // Create SpielWindow and display it
                     try {
                         SpielFeld1 = 0;
@@ -248,7 +348,7 @@ public class PanelSize extends JPanel {
             } else {
                 try {
                     menuPanel.setVisible(false);
-                    //menuFrame.dispose();
+                    menuFrame.dispose();
                     new SpielWindow(menuFrame);
                 } catch (IOException | FontFormatException ioException) {
                     ioException.printStackTrace();
@@ -285,6 +385,10 @@ public class PanelSize extends JPanel {
                 "5 ".repeat(Math.max(0, size5));
     }
 
+    /**
+     * Berechnet alle temporären Werte <br>
+     * Updated menuCounter und ButtonApply
+     */
     private void UpdateMenuCounter() {
         temp_fieldsize = menuSlider1.getValue();
         temp_fields = temp_fieldsize * temp_fieldsize;
@@ -324,7 +428,13 @@ public class PanelSize extends JPanel {
         System.out.println("size5: " + temp_size5);
         System.out.println("sumofships: " + sumofships);
     }
-
+    /**
+     * Legt Größe und Position der Components fest
+     * @param comp Ein Swing Component, welcher in constraints embedded werden soll.
+     * @param gridx X-Position auf der X-Achse
+     * @param gridy Y-Position auf der Y-Achse
+     * @param gridwidth Breite des Components
+     */
     private void makeConstraints(JComponent comp, int gridx, int gridy, int gridwidth) {
         constraints.insets = new Insets(0, 5, 5, 5);
         constraints.gridx = gridx;
@@ -337,6 +447,10 @@ public class PanelSize extends JPanel {
         add(comp, constraints);
     }
 
+    /**
+     * Update des Hintergrundbilds
+     * @param g Übergibt Graphics Objekt
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
